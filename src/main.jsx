273a5e3175c509e76443925c0c15bd63d0562bfd
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-01-19 23:39:08 -0600

    Finalizando useContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { HooksApp } from './HooksApp'
@@ -13,12 +14,15 @@ import './index.css'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
-import { TodoApp } from './08-useReducer/TodoApp'
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 
 // import './08-useReducer/intro-reducer'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TodoApp/>
+    <BrowserRouter>
+      <MainApp/>
+    </BrowserRouter>
   </StrictMode>
 )
